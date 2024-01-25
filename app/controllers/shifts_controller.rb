1fc0bf35044be16d8fa9643579072c61c58176fc
Devin Pile <devinpile@gmail.com>
2024-01-25 16:17:34 -0700

    Add the same for the other parent class

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -2,4 +2,8 @@ class ShiftsController < ApplicationController
     def index 
         @shifts = Shift.all
     end
+
+    def show 
+        @shift = Shift.find(params[:id])
+    end 
 end
--- app/models/shift.rb
+++ app/models/shift.rb
@@ -4,4 +4,8 @@ class Shift < ApplicationRecord
     validates_presence_of :shift_type
     validates_presence_of :hours
     validates_presence_of :full_staff
+
+    def fully_staffed
+        self.full_staff ? 'yes' : 'no'
+    end
 end 
